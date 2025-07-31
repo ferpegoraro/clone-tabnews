c66a5cd94cc457ba27331b9ad14c5a2e6dba3280
Fernando Pegoraro Bilia <fernandopbilia2304@gmail.com>
2025-07-31 18:36:34 -0300

    ignore , make  more robust to error handling and add quest in the

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  const postgresVersion = responseBody.dependencies.database.postgres_version;
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  const usedConnections = responseBody.dependencies.database.used_connections;
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(postgresVersion).toEqual("16.9");
+  expect(typeof maxConnections).toBe("number");
+  expect(maxConnections).toBeGreaterThan(0);
+  expect(usedConnections).toEqual(1);
 });
