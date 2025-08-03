89eba2fdb18718eb4fee46492c47daae39b1dff8
Fernando Pegoraro Bilia <fernandopbilia2304@gmail.com>
2025-08-03 20:35:18 -0300

    adds migrations scripts

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSL(),
   });
 
   try {
@@ -25,3 +25,7 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSL() {
+  return process.env.NODE_ENV === "development" ? false : true;
+}
